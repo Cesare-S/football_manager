82418e9a66123ec39516ed75f4697803e7c7dcab
Cesare-S <105559532+Cesare-S@users.noreply.github.com>
2026-09-06 21:06:52 +0000

    .

--- Main.java
+++ Main.java
@@ -2,12 +2,21 @@ public class Main {
 
     public static void main() {
     
-        Player player = new Player("Kevin", "De Bruyne", 32, 89, "T");
-        System.out.println(player.nuovaVersioneGiocatore());
-        player.increaseAge();
-        player.increaseOverall(0, 0);
+        Player midfielder = new Player("Kevin", "De Bruyne", 32, 89, "T");
+        System.out.println(midfielder.nuovaVersioneGiocatore());
+        
+        Team team = new Team("Napoli");
 
-        System.out.println(player.nuovaVersioneGiocatore());
+        team.insertPlayerOnTeam(midfielder);
+
+        Player goalkeeper = new Player("Alex", "Meret", 28, 83, "Por");
+        System.out.println(goalkeeper.nuovaVersioneGiocatore());
+
+        team.insertPlayerOnTeam(goalkeeper);
+
+
+        System.out.println(team.getPlayers());
+
     }
    
 
