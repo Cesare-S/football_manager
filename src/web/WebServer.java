d0cbc396f76c1d44250d55aa61b7425200677209
Cesare-S <105559532+Cesare-S@users.noreply.github.com>
2026-09-07 15:14:19 +0000

    .

--- src/web/WebServer.java
+++ src/web/WebServer.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import model.Club;
 import model.Team;
 import model.Player;
@@ -36,9 +39,30 @@ public class WebServer {
                 try {
                 String response = "<h1>Benvenuto nella pagina del club:"+club.getName()+"!</h1>";
                 
+                response += "<h3>Il tema del club è: "+team.getName()+"</h3>";        
+
+
+                response += "<span>I giocatori sono:";
+                List<Player> players = team.getPlayers();
+                for (int i = 0; i < players.size(); i++) {
+                    
+    
+                    Player player = players.get(i);
+                    // Accedi alle proprietà tramite il punto + il metodo getter
+                    String nome = player.getName(); 
+                    String cognome = player.getSurname();
+                    int age = player.getAge();
+                    int overall = player.getOverall();
+                    String role = player.getRole();
+
+                    response += "<p>"+nome+" "+cognome+". Età: "+age+". Valutazione: "+overall+". Con il ruolo di: "+role+"</p>"; 
+                
+                }
+
                 exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
                 
                 exchange.sendResponseHeaders(200, response.getBytes().length);
+
                 
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
