89f68a6552299aebfd50016b94935497851777f7
Cesare-S <105559532+Cesare-S@users.noreply.github.com>
2026-09-08 13:41:56 +0000

    add manager class

---
+++ src/main/java/model/Manager.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Manager{
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private long budgetEur;
+
+
+    public Manager(String formName, String formEmail, String formPassword) {
+
+        if (formName == null || formName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Il nome non può essere vuoto.");
+        }
+
+        if (formEmail == null || formEmail.trim().isEmpty()) {
+            throw new IllegalArgumentException("Email non può essere vuoto.");
+        }
+
+        if (formPassword == null || formPassword.trim().isEmpty()) {
+            throw new IllegalArgumentException("Password non può essere vuoto.");
+        }
+
+        this.name = formName;
+        this.email = formEmail;
+        this.password = formPassword;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public long getBudgetEur(){
+        return budgetEur;
+    }
+
+
+}
---
+++ src/main/java/persistence/Database.java
@@ -0,0 +1,31 @@
+package persistence;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+    public static Connection connection() throws SQLException {
+        String url = "jdbc:sqlite:data/football_manager.db";
+
+        Connection conn = DriverManager.getConnection(url);
+        if (conn != null){
+
+            try(Statement stmt = conn.createStatement()){
+                stmt.execute("PRAGMA foreign_keys = ON;");    
+            }
+            
+            
+
+            return conn;
+        }
+        else
+            throw new SQLException("Errore: la connessione passata è nulla!");
+
+        
+            
+       
+    }
+}
